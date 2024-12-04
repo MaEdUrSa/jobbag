f0a1ad07df2dcb0b8d12f6ad7da7a876dbf5da36
meus <meus67@gmail.com>
2024-12-03 21:23:50 -0300

    Prueba Mod-6 jobbag

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy, through: :reactions
+  
+  has_one_attached :avatar
+  has_many_attached :images
+
+  has_many :images, as: :imageable
+  has_and_belongs_to_many :posts
+
+  has_many :reactions
+
+  enum :role, [:normal_user, :admin]
+end
